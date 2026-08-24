faf736bdbfb2ed8977929c6c1839a8182c89a39b
ghryslt-cmyk <ghryslt@gmail.com>
2026-08-24 20:22:13 +0700

    Move SeasonalSidebars to bottom of DOM for shadow visibility

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ import SeasonalSidebars from './components/SeasonalSidebars';
 function AppContent() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <SeasonalSidebars />
       <div className="relative z-10 shadow-[8px_0_32px_rgba(0,0,0,0.8),-8px_0_32px_rgba(0,0,0,0.8)] dark:shadow-[8px_0_32px_rgba(0,0,0,0.9),-8px_0_32px_rgba(0,0,0,0.9)]">
         <Navbar />
         <Routes>
@@ -36,6 +35,7 @@ function AppContent() {
           <Route path="/terms" element={<Terms />} />
         </Routes>
       </div>
+      <SeasonalSidebars />
     </div>
   );
 }
