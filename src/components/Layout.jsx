feb8e47230878c8023d5c6b31a34b4806a62fd10
ghryslt-cmyk <ghryslt@gmail.com>
2026-08-30 20:05:27 +0700

    Update side gutters size and top gutter transparency
    - Update left gutter size to match old sidebar: w-40 lg:w-48 xl:w-56
    - Update right gutter size to match old sidebar: w-48 lg:w-64
    - Reduce top gutter height: h-10 md:h-12 lg:h-14 (smaller than before)
    - Increase top gutter transparency: opacity-40 (more transparent to show full-img behind)
    - Remove top-gutter-transparent class since we're using inline opacity

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = memo(({ children }) => {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-all duration-300 relative">
       {/* Fixed Side Gutters - Full Screen Height */}
-      <div className="fixed left-0 top-0 bottom-0 w-48 xl:w-56 z-10 hidden lg:block">
+      <div className="fixed left-0 top-0 bottom-0 w-40 lg:w-48 xl:w-56 z-10 hidden lg:block">
         <div 
           className="w-full h-screen bg-cover bg-center"
           style={{ backgroundImage: "url('/full-img.png')" }}
@@ -14,7 +14,7 @@ const Layout = memo(({ children }) => {
         </div>
       </div>
 
-      <div className="fixed right-0 top-0 bottom-0 w-48 xl:w-56 z-10 hidden lg:block">
+      <div className="fixed right-0 top-0 bottom-0 w-48 lg:w-64 z-10 hidden lg:block">
         <div 
           className="w-full h-screen bg-cover bg-center"
           style={{ backgroundImage: "url('/full-img.png')" }}
@@ -26,7 +26,7 @@ const Layout = memo(({ children }) => {
       {/* Main Content Area with Top Gutter */}
       <div className="flex max-w-[1600px] mx-auto relative z-20">
         {/* Spacer for left gutter */}
-        <div className="hidden lg:block w-48 xl:w-56 flex-shrink-0"></div>
+        <div className="hidden lg:block w-40 lg:w-48 xl:w-56 flex-shrink-0"></div>
         
         {/* Main content */}
         <div className="flex-1 min-w-0">
@@ -35,7 +35,7 @@ const Layout = memo(({ children }) => {
         </div>
         
         {/* Spacer for right gutter */}
-        <div className="hidden lg:block w-48 xl:w-56 flex-shrink-0"></div>
+        <div className="hidden lg:block w-48 lg:w-64 flex-shrink-0"></div>
       </div>
     </div>
   );
--- src/components/TopGutter.jsx
+++ src/components/TopGutter.jsx
@@ -2,11 +2,11 @@ import { memo } from 'react';
 
 const TopGutter = memo(() => {
   return (
-    <div className="w-full h-16 md:h-20 lg:h-24 relative overflow-hidden top-gutter-transparent">
+    <div className="w-full h-10 md:h-12 lg:h-14 relative overflow-hidden">
       <img 
         src="/top-gutter.png" 
         alt="Top Gutter" 
-        className="w-full h-full object-cover opacity-80"
+        className="w-full h-full object-cover opacity-40"
         style={{ objectPosition: 'bottom' }}
       />
       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-gray-50 dark:to-gray-900"></div>
